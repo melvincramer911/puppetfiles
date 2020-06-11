8b3151e8c18c7de24e773620d0eaf432670e28e1
melvincramer911 <davidbr@davidbr.com>
2020-06-11 13:37:28 -0400

    Update getmenu.sh

--- getmenu.sh
+++ getmenu.sh
@@ -5,3 +5,7 @@ cd ~
 curl https://raw.githubusercontent.com/melvincramer911/puppetfiles/master/menu.sh  >> menu.sh
 chmod 700 ./menu.sh 
 
+curl https://raw.githubusercontent.com/melvincramer911/timfox123/master/Puppetmasterbuild >> buildmaster.sh
+chmod 700 ./buildmaster.sh
+
+
