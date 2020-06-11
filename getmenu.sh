f1b6c0ead6897edffd658e2856fa181f79122eec
melvincramer911 <davidbr@davidbr.com>
2020-06-11 14:12:09 -0400

    Update getmenu.sh

--- getmenu.sh
+++ getmenu.sh
@@ -8,4 +8,7 @@ chmod 700 ./menu.sh
 curl https://raw.githubusercontent.com/melvincramer911/timfox123/master/Puppetmasterbuild >> buildmaster.sh
 chmod 700 ./buildmaster.sh
 
+curl https://github.com/melvincramer911/timfox123/edit/master/puppetMasterCreatewithpdk  >> pdkmodules.sh 
+chmod 700 ./buildmaster.sh
+
 
