267a2ac7ba2bc0d4c55f2e8a6c67298ef08ca100
melvincramer911 <davidbr@davidbr.com>
2020-06-15 18:34:44 -0400

    Update getmenu.sh

--- getmenu.sh
+++ getmenu.sh
@@ -14,4 +14,10 @@ chmod 700 ./pdkmodules.sh
 curl https://raw.githubusercontent.com/melvincramer911/timfox123/master/puppetminionbuild  >> buildminion.sh
 chmod 700 ./buildminion.sh  
 
+curl https://raw.githubusercontent.com/melvincramer911/puppetWithParms/master/deployppfilesParams.sh >> deployppfilesParams.sh 
+chmod 700 ./deployppfilesParams.sh
+
+
+
+
 
